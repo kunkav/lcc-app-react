0e671f624a2fc843e8d462c9c6b9fc70bc77f4c0
Kunal Kavimandan <Kunal.Kavimandan@ustpe.com>
2024-01-18 15:40:28 +0530

    Removed amplify ui code

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {
-  LoginPage 
- } from './ui-components';
+
 
 export default function App() {
   return (
--- index.js
+++ index.js
@@ -8,13 +8,10 @@ import App from './App';
 
 
 //configuring amplify ui to be used in this app
-import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 
 import awsconfig from './aws-exports';
 
-import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./ui-components";
 
 Amplify.configure(awsconfig);
 
